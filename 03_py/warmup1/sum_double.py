cbe9fa03e84551be9fc30ad0785a4da64882e95a
Hui Wang <88509047+hwang30git@users.noreply.github.com>
2022-09-21 09:14:38 -0400

    warmup 1

---
+++ 03_py/warmup1/sum_double.py
@@ -0,0 +1,16 @@
+# https://codingbat.com/prob/p141905
+
+"""
+Given two int values, return their sum. Unless the two values are the same, then
+return double their sum.
+"""
+def sum_double(a, b):
+    if(a == b):
+        return 2 * (a + b)
+    return (a + b)
+
+# test cases: do not edit
+print(sum_double(1, 2)) # 3
+print(sum_double(3, 2)) # 5
+print(sum_double(2, 2)) # 8
+
