32338bdbf91fcc48d0cd9c9d15a4169d45bee671
Hui Wang <88509047+hwang30git@users.noreply.github.com>
2022-09-30 09:22:05 -0400

    read csv

--- 06_py-csv/numbercruncher.py
+++ 06_py-csv/numbercruncher.py
@@ -8,10 +8,21 @@ Time Spent:
 
 '''
 DISCO:
-
+random has a built in function random.choices which weights the chances of getting elements from an array
 QCC:
 
 HOW THIS SCRIPT WORKS: 
 
 '''
 
+import random
+import csv
+
+with open('occupations.csv', newline='') as csvfile:
+    occupations = list(csv.reader(csvfile))
+
+print(occupations)
+
+jobs = {}
+for i in range(1, len(occupations) - 1):
+    
