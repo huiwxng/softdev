dbcf70efca03dc58524c06484e995e5473c6156f
Hui Wang <xeraonomen@gmail.com>
2022-10-02 19:44:21 -0400

    update numbercruncher.py

--- 06_py-csv/numbercruncher.py
+++ 06_py-csv/numbercruncher.py
@@ -1,28 +1,45 @@
-'''
-Duo XeraRedStar: Hui Wang, William Vongphanith
-SoftDev
-K05 -- Bitstream
-2022-09-28
-Time Spent: 
-'''
+"""
+Hui Wang, William Vongphanith
+Softdev
+K06 -- py-csv
+2022-09-29
+Time Spent: 0.3 hour
+"""
 
-'''
+"""
 DISCO:
-random has a built in function random.choices which weights the chances of getting elements from an array
+    - The csv module is very useful for reading csv files (when you don't want to parse them yourself)
+    - You can use the csv.DictReader() function to read a csv file into a dictionary (instead of a list of lists)
+    
 QCC:
+    - Can you use the csv module to write to a csv file from a dictionary?
+    
+OPS SUMMARY:
+    - We used the csv module to read the csv file
+    - We used the random module to generate a random number
+    - We used the random number to select a job from the csv file
+    - We printed the job
+"""
 
-HOW THIS SCRIPT WORKS: 
-
-'''
-
-import random
 import csv
+import random
 
-with open('occupations.csv', newline='') as csvfile:
-    occupations = list(csv.reader(csvfile))
+def main():
+    with open('occupations.csv', newline='') as csvfile:
+        # read that dict
+        reader = csv.DictReader(csvfile)
+        # jobs list
+        jobs = []
+        # for each job
+        for row in reader:
+            if row['Job Class'] != 'Total':
+                # add the job to the list percentage x 10 times (weighting to the first decimal place)
+                for i in range(int(float(row['Percentage']) * 10)):
+                    # add the job to the list
+                    jobs.append(row['Job Class'])
+        # print a random job (they're weighted so no need to do arcane probability stuff)
+        print(random.choice(jobs))
 
-print(occupations)
-
-jobs = {}
-for i in range(1, len(occupations) - 1):
-    jobs[occupations[0][0]] = occupations[0][1]
+# run main
+if __name__ == "__main__":
+    main()
