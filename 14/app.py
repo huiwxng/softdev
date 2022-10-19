94644bc5954c369f259ac51fedb11b84c051cf5c
Hui Wang <88509047+hwang30git@users.noreply.github.com>
2022-10-19 09:11:33 -0400

    +14

---
+++ 14/app.py
@@ -0,0 +1,17 @@
+# notes-and-code: Hui Wang, Ryan Lau, Jonathan Song
+# SoftDev
+# Oct 2022
+
+# DEMO 
+# basics of /static folder
+
+from flask import Flask, render_template
+app = Flask(__name__) 
+
+@app.route("/")       
+def hello_world():
+    return render_template("index.html")
+
+if __name__ == "__main__":  # true if this file NOT imported
+    app.debug = True        # enable auto-reload upon code change
+    app.run()
