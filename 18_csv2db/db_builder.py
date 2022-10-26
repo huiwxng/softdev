b9ae20b09115d116efdb1e4b0fad235df43797d0
Hui Wang <88509047+hwang30git@users.noreply.github.com>
2022-10-26 09:35:04 -0400

    new discovery in readme

--- 18_csv2db/db_builder.py
+++ 18_csv2db/db_builder.py
@@ -2,7 +2,7 @@
 # SoftDev  
 # K18 -- SQLITE3 BASICS
 # 2022-10-25
-# time spent:
+# time spent: 0.6 hrs
 
 import sqlite3   #enable control of an sqlite database
 import csv       #facilitate CSV I/O
@@ -18,20 +18,23 @@ c = db.cursor()               #facilitate db ops -- you will use cursor to trigg
 
 # < < < INSERT YOUR TEAM'S POPULATE-THE-DB CODE HERE > > >
 with open('courses.csv') as courses:
-    reader = csv.DictReader(courses) # reads the csv file as a csv.DictReader object
+    dReader = csv.DictReader(courses) # reads the csv file as a csv.DictReader object
     print(list(reader)) # [{'code': 'systems', 'mark': '75', 'id': '1'}, ...]
+    print(list(reader)) # []
+    print(dReader)
+    print(dReader)
     command = "create table courses (code text, mark int, id int)"
     c.execute(command)
-    for row in reader: # iterate through each row in the csv.DictReader object
+    for row in dReader: # iterate through each row in the csv.DictReader object
         command = f"insert into courses values ('{row['code']}', {row['mark']}, {row['id']})"
         print (command)
         c.execute(command)
 
 with open('students.csv') as students:
-    reader = csv.DictReader(students)
+    dReader = csv.DictReader(students)
     command = "create table students (name text, age int, id int)"
     c.execute(command)
-    for row in reader:
+    for row in dReader:
         command = f"insert into students values ('{row['name']}', {row['age']}, {row['id']})"
         print (command)
         c.execute(command)
