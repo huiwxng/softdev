95ae81cf0436f13ffb10b5b22d18e5de9cdd2209
Hui Wang <88509047+hwang30git@users.noreply.github.com>
2022-11-22 09:28:02 -0500

    +20

--- 20_restapi/app.py
+++ 20_restapi/app.py
@@ -8,8 +8,11 @@ def render_img():
     f = open("key_nasa.txt", "r") # opens key_nasa.txt
     api_key = f.read() # reads the api key
     response = requests.get(f"https://api.nasa.gov/planetary/apod?api_key={api_key}") # gets from the url
+    # print (response)
     data = response.text # changes into readable json
+    # print(data)
     data_dict = json.loads(data) # loads the json into a dictionary
+    # print(data_dict)
     return render_template("main.html", title=data_dict["title"], image=data_dict["url"], desc=data_dict["explanation"]) # renders the page
 
 if __name__ == "__main__":  # true if this file NOT imported
