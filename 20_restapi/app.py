5260581f6cbc42a700474076bb42134f258bb771
Hui Wang <88509047+hwang30git@users.noreply.github.com>
2022-11-22 09:25:09 -0500

    20

--- 20_restapi/app.py
+++ 20_restapi/app.py
@@ -1,10 +1,17 @@
 from flask import Flask, render_template, request
-import request, json
+import requests, json
 
 app = Flask(__name__)
 
 @app.route("/")
 def render_img():
-    f = open("key_nasa.txt", "r")
-    api_key = f.read()
-    response = requests.get(f"https://api.nasa.gov/planetary/apod?api_key={api_key}")
+    f = open("key_nasa.txt", "r") # opens key_nasa.txt
+    api_key = f.read() # reads the api key
+    response = requests.get(f"https://api.nasa.gov/planetary/apod?api_key={api_key}") # gets from the url
+    data = response.text # changes into readable json
+    data_dict = json.loads(data) # loads the json into a dictionary
+    return render_template("main.html", title=data_dict["title"], image=data_dict["url"], desc=data_dict["explanation"]) # renders the page
+
+if __name__ == "__main__":  # true if this file NOT imported
+    app.debug = True        # enable auto-reload upon code change
+    app.run()
